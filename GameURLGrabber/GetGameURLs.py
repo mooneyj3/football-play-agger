47ef295a810190100e95c3185321de0e69173bae
mooneyj3 <mooneyj3@wwu.edu>
2017-07-18 13:51:22 -0700

    began implementing urllib and bs4

--- GameURLGrabber/GetGameURLs.py
+++ GameURLGrabber/GetGameURLs.py
@@ -1,6 +1,22 @@
+# include bs4 library
 from bs4 import BeautifulSoup as Soup
 
-def myfunction():
-    print("Hello World!");
+# include urllib library
+import urllib.request
+response = urllib.request.urlopen('http://www.nfl.com/scores/2016/REG17')
 
-myfunction();
+html = response.read()
+
+soup = Soup(html, "lxml")
+
+print(soup.prettify())
+
+
+
+## soup = Soup('http://www.nfl.com/scores/2016/REG17', 'html.parser')
+
+##print(soup.prettify())
+
+# def myfunction():
+#     print("Hello World!");
+# myfunction();
