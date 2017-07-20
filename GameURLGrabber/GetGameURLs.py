6be1f0d5d038494ab3713a2b7ef3135b5513872c
Jonathan Mooneyham <mooneyj3@wwu.edu>
2017-07-20 08:56:47 -0700

    some new project build

--- GameURLGrabber/GetGameURLs.py
+++ GameURLGrabber/GetGameURLs.py
@@ -3,19 +3,29 @@ from bs4 import BeautifulSoup as Soup
 
 # include urllib library
 import urllib.request
+
 response = urllib.request.urlopen('http://www.nfl.com/scores/2016/REG17')
 
 html = response.read()
 
 soup = Soup(html, "lxml")
 
-print(soup.prettify())
+##print(soup.prettify())
+##print(soup.find_all('a'))
 
+##print(soup.a['game-center-link'])
 
+## <div class="highlight-link">
+## <a href="/gamecenter/2017010100/2016/REG17/saints@falcons#menu=highlights|contentId:0ap3000000767814&amp;tab=analyze">
 
 ## soup = Soup('http://www.nfl.com/scores/2016/REG17', 'html.parser')
 
 ##print(soup.prettify())
+
+soup.findall("a", {"class" : "class=game-center-link"})
+
+
+##print(soup.findall("a", { "class" : "class" : "game-center-link"}))
 
 # def myfunction():
 #     print("Hello World!");
