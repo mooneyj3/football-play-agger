1b363aaf6e10139e5b267c3882a7296560d567ca
Jonathan Mooneyham <mooneyj3@wwu.edu>
2017-07-20 14:07:39 -0700

    updates to GetGameURLs

--- GameURLGrabber/GetGameURLs.py
+++ GameURLGrabber/GetGameURLs.py
@@ -1,7 +1,8 @@
 """
 GetGameURLs.py
 
-Collects and aggregates NFL gamecenter URL's
+Collects and aggregates NFL gamecenter URL's.
+Each game has a unique game center URL
 Gamecenter URL's have the following format:
     http://www.nfl.com/scores/YYYY/SSNWK
 
@@ -18,8 +19,6 @@ from bs4 import BeautifulSoup as Soup
 import urllib.request
 
 # class variables
-startYear = 2013
-endYear = 2017
 home = "http://www.nfl.com/scores/"
 
 """
@@ -35,13 +34,25 @@ for i in soup.find_all("a", class_="game-center-link", href=True):
     print(i['href'])
 """
 
-for year in range(startYear, endYear):
+# callee to generate Game URL files
+def generateGameURLs(startYear, endYear):
+    for year in range(startYear, endYear):
 
-    # get PRESEASON URL's for current year
-    for ssn in range(1, 4):
-        url = home + str(year) + "/PRE" + str(ssn)
-        response = urllib.request.urlopen(url)
-        html = response.read()
-        soup = Soup(html, "lxml")
-        for i in soup.find_all("a", class_="game-center-link", href=True):
-            print(i['href'])
+        # get PRE season URL's for current year
+        for week in range(1, 4):
+            url = home + str(year) + "/PRE" + str(week)
+            response = urllib.request.urlopen(url)
+            html = response.read()
+            soup = Soup(html, "lxml")
+            for i in soup.find_all("a", class_="game-center-link", href=True):
+                print(i['href'])
+                
+        # get REG season URL's for current year
+        for week in range(1, 17):
+            url = home + str(year) + "/REG" + str(week)
+        
+        # get POST season URL's for current year
+        for week in range(18, 22):
+            url = home + str(year) + "/POST" + str(week)
+                
+        
